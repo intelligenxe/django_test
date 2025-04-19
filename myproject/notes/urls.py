e65a3cd34ee2a38f3c1025900c3dfaa69337104a
intelligenxe <drivas@intelligenxe.com>
2025-04-19 11:47:55 -0600

    add/edit_ing urls*2 views serializers

---
+++ myproject/notes/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+from .views import NoteListCreateView, NoteDetailView
+
+urlpatterns = [
+    path('notes/', NoteListCreateView.as_view()),
+    path('notes/<int:pk>/', NoteDetailView.as_view()),
+]
--- myproject/notes/views.py
+++ myproject/notes/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render    #this one came with the django startproject command
 
-# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Note
+from .serializers import NoteSerializer
+from django.shortcuts import get_object_or_404
+
+class NoteListCreateView(APIView):
+    def get(self, request):
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class NoteDetailView(APIView):
+    def get(self, request, pk):
+        note = get_object_or_404(Note, pk=pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        note = get_object_or_404(Note, pk=pk)
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        note = get_object_or_404(Note, pk=pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
