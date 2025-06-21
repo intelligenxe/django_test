fa6c908c156a1a22c5e823b28d2349d404239858
intelligenxe <intelligenxe@gmail.com>
2025-06-21 11:03:26 -0600

    cleaned up  code

--- myproject/notes/models.py
+++ myproject/notes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/', default='images/default.png')  # Image saved to /media/images/
+    # image = models.ImageField(upload_to='images/', default='images/default.png')  # Image saved to /media/images/
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
